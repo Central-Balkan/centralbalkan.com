c89b67f2856312819c579d3fc0de8f129166ca2b
Ivaylo Donchev <ivaylo.i.donchev@gmail.com>
2026-03-15 11:18:42 +0200

    Add more contentX

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,15 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <h1 className="mt-4 text-4xl font-bold tracking-tight text-lightPrimary  sm:text-5xl z-10">
-          Централен Балкан
-        </h1>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
+      <main className="w-full max-w-5xl sm:items-start">
+        <div className="flex gap-5 justify-center min-h-screen flex-col items-center py-32 px-16">
+          <h1 className="text-5xl font-bold tracking-tight text-lightPrimary sm:text-7xl z-10">
+            Централен Балкан
+          </h1>
+          <h2 className="text-xl font-normal text-lightPrimary dark:text-lightSecondary z-10">
+            30 години опит в производството на метални изделия
+          </h2>
+        </div>
       </main>
     </div>
   );
