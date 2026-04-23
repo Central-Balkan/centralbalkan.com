75e1cdd03324e759355c0a959463f335ec15b9cf
Ivo Donchev <ivaylo.i.donchev@gmail.com>
2026-04-23 17:18:23 +0300

    Add logo

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -47,9 +47,9 @@ const Navbar = () => {
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <Link href="/" className="flex shrink-0 items-center">
               <img
-                alt="Your Company"
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                className="h-8 w-auto"
+                alt="Central Balkan Logo"
+                src="/cb_logo.svg"
+                className="h-12 w-auto"
               />
             </Link>
           </div>
